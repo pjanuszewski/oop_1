82ff9ce195bd6324c69dcbb33690ed48e6ea633d
Pawel Januszewski <akurat444@gmail.com>
2023-11-25 15:44:23 +0100

    Problem_2 solved

--- main.cpp
+++ main.cpp
@@ -5,8 +5,19 @@
 struct Wektor2D
 {
     public:
-        double x = 4.028;
-        double y = 1.947;
+        double x;
+        double y;
+        std::string destructorMessage;
+    Wektor2D(double a, double b, const std::string& dMsg) : destructorMessage(dMsg)
+    {
+        x = a;
+        y = b;
+        std::cout << "Constructor activated: " << x << "    " << y << '\n';
+    }
+    ~Wektor2D()
+    {
+        std::cout << "\n" << "Destructor activated: " << destructorMessage;
+    }
     double norm()
     {
         double n = sqrt(x*x + y*y);
@@ -20,8 +31,13 @@ struct Wektor2D
 };
 
 int main(){
-    Wektor2D a;
-    a.norm();
-    a.print();
-    return 0;
+    Wektor2D first_vector{4.028, 6.932, "First"};
+    Wektor2D second_vector{2.352, 6.321, "Second"};
+    Wektor2D third_vector{9.234, 0.624, "Third"};
+    first_vector.norm();
+    first_vector.print();
+    second_vector.norm();
+    second_vector.print();
+    third_vector.norm();
+    third_vector.print();
 }
