b00492ba3e103196570728c441bb852729011721
Pawel Januszewski <akurat444@gmail.com>
2023-11-26 15:36:09 +0100

    Problem_7 solved - adding static members to a class

--- main.cpp
+++ main.cpp
@@ -21,33 +21,11 @@ struct Wektor2D
 public:
     Informer informer;                                          // Create object of class Informer
     std::string destructorMessage;
-    void set_x(double a)
-    {
-        x = a;
-    }
-    void set_y(double a)
-    {
-        y = a;
-    }
-    double get_x() const 
-    {
-        return x;
-    }
-    double get_y() const 
-    {
-        return y;
-    }
-
-    Wektor2D(const std::string& dMsg) : destructorMessage(dMsg) // Constructor #1 with custom message for the destructor
-    {
-        std::cout << "Constructor activated";
-    }
-    Wektor2D() : x(0.0), y(0.0) {}                              // Constructor #2 Default constructor with no parameters
-    Wektor2D(double a, double b) : x(a), y(b) {}                // Constructor #3 with (X,Y) parameters of vector
-    ~Wektor2D()                                                 // Destructor with message from Constructor #1
-    {
-        std::cout << "\n" << "Destructor activated: " << destructorMessage;
-    }
+    void set_x(double a)  {x = a;}
+    void set_y(double a)  {y = a;}
+    double get_x() const  {return x;}
+    double get_y() const  {return y;}
+    double get_num_wek() const {return num_wek;}
     double norm()                                               // Calculates norm of the vector
     { 
         double n = sqrt(x*x + y*y);
@@ -58,10 +36,32 @@ public:
     {
         std::cout << '\n' << x << '\n' << y << '\n';        
     }
+    
+    //Constructors
+    Wektor2D(const std::string& dMsg) : destructorMessage(dMsg) // Constructor #1 with custom message for the destructor
+    {
+        std::cout << "Constructor activated";
+        ++num_wek;
+    }
+
+    Wektor2D() : x(0.0), y(0.0) {++num_wek;}                              // Constructor #2 Default constructor with no parameters
+
+    Wektor2D(double a, double b) : x(a), y(b)                   // Constructor #3 with (X,Y) parameters of vector
+    {
+        ++num_wek;
+    }                
+    
+    //Destructor
+    ~Wektor2D()                                                 // Destructor with message from Constructor #1
+    {
+        std::cout << "\n" << "Destructor activated: " << destructorMessage;
+        --num_wek;
+    }
 
     private:    // x and y are kept private, while only set() and get() are public
         double x;
         double y;
+        static int num_wek;
 };
 
 Wektor2D operator+(const Wektor2D& w1, const Wektor2D& w2)
@@ -76,18 +76,27 @@ Wektor2D operator*(const Wektor2D& w1, const Wektor2D& w2)
     double y1 = w1.get_y() * w2.get_y();
     return Wektor2D(x1, y1);
 }
-std::ostream& operator<<(std::ostream& os, const Wektor2D& vector) {
+std::ostream& operator<<(std::ostream& os, const Wektor2D& vector) 
+{
     os << "x: " << vector.get_x() << ", y: " << vector.get_y();
     return os;
 }
 
+
+int Wektor2D::num_wek=0;
+
 int main()
 {
-    Wektor2D first(4, 2);
-    Wektor2D second(3, 9);
-    Wektor2D sum = first + second;
-    Wektor2D il = first*second;
-    first.print();
+    {
+        Wektor2D first(4, 2.38);        //Brackets make this vector a local variable
+    }                                        //After it goes out of scope, destructor is called
+    Wektor2D second(3.5, 9);
+    Wektor2D third(7, 5.25);
+    //Wektor2D sum = first + second;
+    //Wektor2D il = first*second;
+    //first.print();
     second.print();
-    std::cout << '\n' << sum << '\n' << il << '\n';
+    third.print();
+    std::cout << '\n' << third.get_num_wek();
+    //std::cout << '\n' << sum << '\n' << il << '\n';
 }
