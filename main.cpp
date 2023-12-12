22b8044d7a260bc221cd8d52f1bf36eb4a41db2e
Pawel Januszewski <akurat444@gmail.com>
2023-12-12 11:01:29 +0100

    Initial Class_3 commit. Up to problem 2 solved.

--- main.cpp
+++ main.cpp
@@ -3,244 +3,31 @@
 #include <iostream>
 #include <math.h>
 #include <string>
-#include "Wektor2D.h"
-#include "Informer.h"
 
-struct Kokardka
+class Figura
 {
-    Kokardka()      { dlugosc = 42; }
-    Kokardka(int d) { dlugosc = d; }
-    
-    int dlugosc;
-};
-
-struct Prezent
-{
-    Prezent(int dk)
+public:
+    Figura() : pole(0) {};
+    Figura(const double &pole) : pole(pole){}; 
+    double get_pole() const
     {
-        std::cout << k.dlugosc << std::endl;
-        k.dlugosc = dk;
-        std::cout << k.dlugosc << std::endl;
-    }
-    
-    Kokardka k;
-    // Inne pola ...
-};
+        return pole;
+    };
+    void set_pole(double p)
+    {
+        pole = p;
+    };
 
-struct Liczba
-{
-    Liczba(int w)           : wartosc{w}         { 
-        std::cout << "Konstruktor z argumentem: " << w << std::endl;
-    }
-    /* Liczba(const Liczba& l) : wartosc{l.wartosc} {
-        std::cout << "Konstruktor kopiujacy: " << wartosc << std::endl;
-    } */
-    
-    int wartosc;
-};
+    ~Figura() 
+    {
 
-class Wektor
-{
-    public:
-        //double *wektor;
-        Wektor(const int& n, const int& length) : capacity(n), wektor(new double[n]), vector_size(length)
-        {
-            for(int i=0; i < n; i++)
-            {
-                wektor[i] = 0.0;
-            }
-        }
+    };
 
-        Wektor(const Wektor& w) : capacity(w.capacity), wektor(new double[w.capacity]), vector_size(w.vector_size)
-        {
-            for(int i=0; i < w.capacity; i++)
-            {
-                wektor[i] = w.wektor[i];
-            }
-        }
-
-        Wektor pow2(Wektor&& w)
-        {
-            Wektor result 
-            {
-                std::move(w)
-            };
-            for(int i=0; i < result.capacity; i++)
-            {
-                result.wektor[i] = result.wektor[i] * result.wektor[i];
-            }
-            return result;
-        }
-
-        ~Wektor()
-        {
-            delete[] wektor;
-        }
-        int get_vector_size() const
-        {
-            return vector_size;
-        }
-        void print() const 
-        {
-            for (int i = 0; i < vector_size; i++) 
-            {
-                std::cout << '\n' << wektor[i];
-            }
-            std::cout <<'\n';
-        }
-        int get_capacity() const
-        {
-            return capacity;
-        }
-        void change_size(const int &new_size) 
-        {
-            if (new_size <= capacity) 
-            {
-                if (new_size <= vector_size) 
-                {
-                    vector_size = new_size;
-                } 
-                else 
-                {
-                    for (int i = vector_size; i <=new_size; i++) 
-                    {
-                        wektor[i] = 0.0;
-                    }
-                    vector_size = new_size;
-                }
-            } 
-            else 
-            {
-                double *new_wektor = new double[new_size];
-                for (int i = 0; i < vector_size; i++)
-                {
-                    new_wektor[i] = wektor[i];
-                }
-                for (int i = vector_size; i <= new_size; i++) 
-                {
-                    new_wektor[i] = 0.0;
-                }
-                delete [] wektor;
-                wektor = new_wektor;
-                capacity = new_size;
-                vector_size = new_size;
-            }
-        }
-
-        double &operator[](const int &index)
-        {
-            if (index < 0)
-            {
-                std::cout << "Index can't be negative. Index set to 0." << std::endl;
-                return wektor[0];
-            }
-            if (index < vector_size)
-            {
-                return wektor[index];
-            }
-            else 
-            {
-                std::cerr << "Index out of bound. Size increased to: " << index << std::endl;
-                Wektor::change_size(index);
-                return wektor[index];
-            }
-        }
-        Wektor &operator=(const Wektor &b)
-        {
-            if (this == &b)
-            {
-                return *this;
-            }
-            else
-            {
-                delete[] wektor;
-                capacity = b.capacity;
-                wektor = new double[capacity];
-                for (int i = 0; i < capacity; i++)
-                {
-                    wektor[i] = b.wektor[i];
-                }
-                return *this;
-            }
-        }
-        Wektor &operator=(Wektor &&b)
-        {
-            if (this != &b)
-            {
-                delete[] wektor;
-                capacity = b.capacity;
-                wektor = b.wektor;
-                b.wektor = nullptr;
-                b.capacity = 0;
-                b.vector_size = 0;
-            }
-            return *this;
-        }
-    private:
-        double *wektor;
-        unsigned int vector_size;
-        unsigned int capacity;
+private:
+    double pole;
 };
 
 int main()
 {
-/*  double A[5]{13.32, 54.78, 34.01, 81.5, 5.12};
-    Wektor my_vector(10,5);
-    my_vector.print();
-
-    for (int i = 0; i < sizeof(A) / sizeof(A[0]); i++)
-    {
-        my_vector[i] = A[i];
-    }
-    my_vector.print();
-    my_vector.change_size(8);
-    my_vector.print();
-
-    my_vector.change_size(14);
-    my_vector.print();
-
-    std::cout << "Vector size: " << my_vector.get_vector_size() << std::endl;
-    std::cout << "Vector capacity: " << my_vector.get_capacity() << std::endl;
-    my_vector[0] = 42.;
-    my_vector.print();
-    double a = my_vector[0];
-    std::cout << a << '\n';
-    a++;
-    std::cout << a << '\n';
-    double b = my_vector[16];
-    my_vector.print();
-    try {
-        double out_of_bounds = my_vector[-3];  // This should throw an error
-    } catch (const std::out_of_range& e) {
-        std::cout << "Caught an out-of-range exception: " << e.what() << '\n';
-    } */
-/*     Prezent prezent(15);
-    Liczba a{1};
-    Liczba b{a};  // W celu konstrukcji b wołamy konstruktor kopiujący z argumentem 'a'
-    Liczba c = a; // Tutaj także wołamy konstruktor kopiujący, vide lab. 1
-    std::cout << "b: " << b.wartosc << std::endl;
-    std::cout << "c: " << c.wartosc << std::endl; */
-
-    double A[5]{13.32, 54.78, 34.01, 81.5, 5.12};
-    Wektor my_vector(10,5);
-    my_vector.print();
-
- 
-    for (int i = 0; i < sizeof(A) / sizeof(A[0]); i++)
-    {
-        my_vector[i] = A[i];
-    }
-    my_vector.print();
-/*    Wektor copy_vector(my_vector);
-    copy_vector.print(); */
-
-    Wektor w2 = my_vector.pow2(std::move(my_vector));
-    w2.print();
-
-    Wektor w3 = std::move(w2);
-    w3.print();
-    // w2.print(); Unsafe operation, w2 is now empty and should not be used
-    
-
     return 0;
 }
