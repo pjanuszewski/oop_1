6b5d36429c25cfa70935032718de5bda5cccb32c
Pawel Januszewski <akurat444@gmail.com>
2023-12-27 14:58:41 +0100

    Up to task 6 finished

--- main.cpp
+++ main.cpp
@@ -7,12 +7,36 @@
 #include <math.h>
 #include <string>
 #include <vector>
+#include "Human.hpp"
 
 int main()
 {
-    std::vector<int> v{100, 42};
-    std::cout << v.size() << std::endl;
-    std::cout << v.capacity() << std::endl;
-    std::cout << v[99] << std::endl;
+    std::vector<Human> v;
+    std::string name;
+    while (true)
+    {
+        std::cin >> name;
+        if (name == "q")
+        {
+            break;
+        }
+        else if (name == "pop")
+        {
+            if (!v.empty())
+            {
+                v.pop_back();
+            }
+        }
+        else
+        {
+            Human h(name, 0, true, true);
+            v.push_back(std::move(h));
+            std::cout << '\n' << v.back().getName() << '\n';
+        }
+    }
+    for (int i = 0; i < v.size(); ++i)
+    {
+        std::cout << v[i].getName() << '\n';
+    }
     return 0;
 }
