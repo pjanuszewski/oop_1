d9b2bc12af8890abebf1d74a38bd988e515b8372
Pawel Januszewski <akurat444@gmail.com>
2023-12-27 19:23:32 +0100

    Up to task 17 finished. Class_5 completed

--- main.cpp
+++ main.cpp
@@ -1,18 +1,58 @@
-# include "Human.hpp"
-# include <list>
-# include <iostream>
+#include <cmath>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <iterator>
+#include <math.h>
+#include <string>
+#include <vector>
+#include "Human.hpp"
+#include <list>
 
 int main()
 {
-    std::list<int> list;
-    list.push_front(10); // error: no match for ‘operator[]’ (operand types are ‘std::list<int>’ and ‘int’)
-    list.emplace_back(20);
-    list.emplace_front(30);
-    list.emplace_front(23);
-    list.emplace_back(54);
-    std::cout << list.front() << std::endl;
-    std::cout << list.back() << std::endl;
-    std::cout << &list.front() << std::endl;
-    std::cout << &list.back() << std::endl;
+    std::vector<Human> v;
+    std::string name;
+    std::vector<Human>::iterator it = v.begin();
+    std::list<int> l(4);
+    auto it2 = l.begin();
+    std::vector<Human>::const_reverse_iterator rit;
+    std::list<int>::reverse_iterator rit2;
+    l = {1, 2, 3, 4};
+    while (true)
+    {
+        std::cin >> name;
+        if (name == "q")
+        {
+            break;
+        }
+        else if (name == "pop")
+        {
+            if (!v.empty())
+            {
+                v.pop_back();
+            }
+        }
+        else
+        {
+            Human h(name, 0, true, true);
+            v.push_back(std::move(h));
+            std::cout << '\n' << v.back().getName() << '\n';
+        }
+    }
+    for (rit = v.crbegin(); rit != v.crend(); ++rit)
+    {
+        std::cout << rit->getName() << '\n';
+    }
+    for (rit2 = l.rbegin(); rit2 != l.rend(); ++rit2)
+    {
+        (*rit2)++;
+        std::cout << *rit2 << '\n';
+    }
+    for(const int& e : l)
+    {
+        std::cout << e << '\n';
+    }
     return 0;
 }
