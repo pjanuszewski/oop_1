d799f86b02a7632be31cdf0adf077da211e2c3fe
Pawel Januszewski <akurat444@gmail.com>
2023-12-30 16:43:02 +0100

    Up to task 7 finished

--- main.cpp
+++ main.cpp
@@ -1,58 +1,85 @@
-#include <cmath>
-#include <cstddef>
-#include <cstdio>
-#include <cstdlib>
-#include <iostream>
+#include <algorithm>
+#include <concepts>
 #include <iterator>
-#include <math.h>
-#include <string>
+#include <random>
 #include <vector>
-#include "Human.hpp"
-#include <list>
 
-int main()
+/////////////////////////////////////////////////////
+// Przeklej kawalek kodu ponizej. Wymagany C++20!
+namespace detail {
+template <typename T>
+requires std::floating_point<T> || std::integral<T>
+struct rand_dist {};
+
+template <std::integral T>
+struct rand_dist<T> {
+    using type = std::uniform_int_distribution<T>;
+};
+
+template <std::floating_point T>
+struct rand_dist<T> {
+    using type = std::uniform_real_distribution<T>;
+};
+}  // namespace detail
+
+template <typename T>
+requires std::floating_point<T> || std::integral<T>
+using rand_dist_t = typename detail::rand_dist<T>::type;
+
+//
+template <typename T>
+requires std::floating_point<T> || std::integral<T>
+auto makeRandomVector(std::size_t size, T min, T max) {
+    static std::mt19937 prng{std::random_device{}()};
+    rand_dist_t<T> random_dist{min, max};
+
+    std::vector<T> result;
+    result.reserve(size);
+    std::generate_n(std::back_inserter(result), size,
+                    [&] { return random_dist(prng); });
+    return result;
+}
+//////////////////////////////////////////////////////
+
+// Przyklad uzycia
+#include <iostream>
+
+class Foo
 {
-    std::vector<Human> v;
-    std::string name;
-    std::vector<Human>::iterator it = v.begin();
-    std::list<int> l(4);
-    auto it2 = l.begin();
-    std::vector<Human>::const_reverse_iterator rit;
-    std::list<int>::reverse_iterator rit2;
-    l = {1, 2, 3, 4};
-    while (true)
+public:
+    Foo(int a) : a(a) {}
+    int getA() const { return a; }
+    void setA(int a) { this->a = a; }
+    int operator()(int a)
     {
-        std::cin >> name;
-        if (name == "q")
-        {
-            break;
-        }
-        else if (name == "pop")
-        {
-            if (!v.empty())
-            {
-                v.pop_back();
-            }
-        }
-        else
-        {
-            Human h(name, 0, true, true);
-            v.push_back(std::move(h));
-            std::cout << '\n' << v.back().getName() << '\n';
-        }
+        return this->a;
     }
-    for (rit = v.crbegin(); rit != v.crend(); ++rit)
+private:
+    int a;
+};
+
+int main() 
+{
+    constexpr auto print_vector = [](const auto& vec) {
+        for (const auto& el : vec) std::cout << el << ' ';
+        std::cout << '\n';
+    };
+    auto v1 = makeRandomVector<int>(12, 0, 10);
+    print_vector(v1);
+/*    std::sort(v1.begin(), v1.end());
+    print_vector(v1);
+    std::cout << '\n' << std::count(v1.begin(), v1.end(), 7) << '\n';
+    std::sort(v1.begin(), v1.end(), std::greater<>{});
+    print_vector(v1);
+    std::cout << '\n' << std::count_if(v1.begin(), v1.end(), [](auto el) { return el > 7;}) << '\n';
+    Foo obiekt([]()
     {
-        std::cout << rit->getName() << '\n';
+        int temp;
+        std::cin >> temp;
+        return temp;
     }
-    for (rit2 = l.rbegin(); rit2 != l.rend(); ++rit2)
-    {
-        (*rit2)++;
-        std::cout << *rit2 << '\n';
-    }
-    for(const int& e : l)
-    {
-        std::cout << e << '\n';
-    }
-    return 0;
+    ());
+    std::cout << std::count_if(v1.begin(), v1.end(), [obiekt](auto el) { return el > obiekt.getA();}) << '\n'; */
+    auto val = [](){int v; std::cin >> v; return v;}();
+    std::cout << std::count_if(v1.begin(), v1.end(), [&](auto el){return el > val;}) << '\n';
 }
